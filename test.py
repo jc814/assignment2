18f188e64b8acc43ea311935ad3c3ba9dc0a68da
hzy <1054539066@qq.com>
2019-06-27 16:51:59 +0800

    change

--- test.py
+++ test.py
@@ -1,117 +1,159 @@
-# As usual, a bit of setup
-from __future__ import print_function
-
-from builtins import print
-
+import tensorflow as tf
 import numpy as np
+import math
+import timeit
 import matplotlib.pyplot as plt
-from cs231n.classifiers.cnn import *
 
-from cs231n.data_utils import get_CIFAR10_data
-from cs231n.gradient_check import eval_numerical_gradient_array, eval_numerical_gradient
-from cs231n.layers import *
-from cs231n.fast_layers import *
-from cs231n.solver import Solver
+from cs231n.data_utils import load_CIFAR10
 
-plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
-plt.rcParams['image.interpolation'] = 'nearest'
-plt.rcParams['image.cmap'] = 'gray'
+def get_CIFAR10_data(num_training=49000, num_validation=1000, num_test=10000):
+    """
+    Load the CIFAR-10 dataset from disk and perform preprocessing to prepare
+    it for the two-layer neural net classifier. These are the same steps as
+    we used for the SVM, but condensed to a single function.
+    """
+    # Load the raw CIFAR-10 data
+    cifar10_dir = 'cs231n/datasets/cifar-10-batches-py'
+    X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
 
-# for auto-reloading external modules
-# see http://stackoverflow.com/questions/1907993/autoreload-of-modules-in-ipython
+    # Subsample the data
+    mask = range(num_training, num_training + num_validation)
+    X_val = X_train[mask]
+    y_val = y_train[mask]
+    mask = range(num_training)
+    X_train = X_train[mask]
+    y_train = y_train[mask]
+    mask = range(num_test)
+    X_test = X_test[mask]
+    y_test = y_test[mask]
+
+    # Normalize the data: subtract the mean image
+    mean_image = np.mean(X_train, axis=0)
+    X_train -= mean_image
+    X_val -= mean_image
+    X_test -= mean_image
+
+    return X_train, y_train, X_val, y_val, X_test, y_test
 
 
-def rel_error(x, y):
-  """ returns relative error """
-  return np.max(np.abs(x - y) / (np.maximum(1e-8, np.abs(x) + np.abs(y))))
 
-# Load the (preprocessed) CIFAR10 data.
 
-data = get_CIFAR10_data()
-for k, v in data.items():
-  print('%s: ' % k, v.shape)
+from tensorflow.python.training import moving_averages
+from tensorflow.python.ops import control_flow_ops
+# clear old variables
+# 清除旧变量
+tf.reset_default_graph()
 
-x_shape = (2, 3, 4, 4)
-w_shape = (3, 3, 4, 4)
-x = np.linspace(-0.1, 0.5, num=np.prod(x_shape)).reshape(x_shape)
-w = np.linspace(-0.2, 0.3, num=np.prod(w_shape)).reshape(w_shape)
-b = np.linspace(-0.1, 0.2, num=3)
+# define our input (e.g. the data that changes every batch)
+# The first dim is None, and gets sets automatically based on batch size fed in
+# 定义输入数据（如每轮迭代中都会改变的数据）
+# 第一维是None，每次迭代时都会根据输入数据自动设定
+X = tf.placeholder(tf.float32, [None, 32, 32, 3])
+y = tf.placeholder(tf.int64, [None])
+is_training = tf.placeholder(tf.bool)
 
-conv_param = {'stride': 2, 'pad': 1}
-out, _ = conv_forward_naive(x, w, b, conv_param)
-correct_out = np.array([[[[-0.08759809, -0.10987781],
-                           [-0.18387192, -0.2109216 ]],
-                          [[ 0.21027089,  0.21661097],
-                           [ 0.22847626,  0.23004637]],
-                          [[ 0.50813986,  0.54309974],
-                           [ 0.64082444,  0.67101435]]],
-                         [[[-0.98053589, -1.03143541],
-                           [-1.19128892, -1.24695841]],
-                          [[ 0.69108355,  0.66880383],
-                           [ 0.59480972,  0.56776003]],
-                          [[ 2.36270298,  2.36904306],
-                           [ 2.38090835,  2.38247847]]]])
+# define model
+# 定义模型
+def complex_model(X,y,is_training):
+    # parameters
+    # 定义一些常量
+    MOVING_AVERAGE_DECAY = 0.9997
+    BN_DECAY = MOVING_AVERAGE_DECAY
+    BN_EPSILON = 0.001
 
-# Compare your output to ours; difference should be around 2e-8
-print('Testing conv_forward_naive')
-print('difference: ', rel_error(out, correct_out))
+    # 7x7 Convolutional Layer with 32 filters and stride of 1
+    # 7x7的卷积窗口，32个卷积核，步长为1
+    Wconv1 = tf.get_variable("Wconv1", shape=[7, 7, 3, 32])
+    bconv1 = tf.get_variable("bconv1", shape=[32])
+    # W: 图像宽度   32
+    # F：Filter的宽度  7
+    # P: padding了多少  0
+    # padding='valid' 就是不padding  padding='same' 自动padding若干个行列使得输出的feature map和原输入feature map的尺寸一致
+    # S: stride 步长  1
+    # (W-F+2P)/S + 1 = (32 - 7 + 2*0)/1 + 1 = 26
+    h1 = tf.nn.conv2d(X, Wconv1, strides=[1,1,1,1], padding='VALID') + bconv1
+    # ReLU Activation Layer
+    # ReLU激活层
+    a1 = tf.nn.relu(h1)  # a1的形状是 [batch_size, 26, 26, 32]
+    # Spatial Batch Normalization Layer (trainable parameters, with scale and centering)
+    # for so-called "global normalization", used with convolutional filters with shape [batch, height, width, depth],
+    # 与全局标准化(global normalization)对应，这里的标准化过程我们称之为局部标准化(Spatial Batch Normalization)。记住，我们的卷积窗口大小是[batch, height, width, depth]
+    # pass axes=[0,1,2]
+    # 需要标准化的轴的索引是 axes = [0, 1, 2]
+    axis = list(range(len(a1.get_shape()) - 1))  # axis = [0,1,2]
+    mean, variance = tf.nn.moments(a1, axis) # mean, variance for each feature map 求出每个卷积结果(feature map)的平均值，方差
 
-from scipy.misc import imread, imresize
+    params_shape = a1.get_shape()[-1:]   # channel or depth 取出最后一维，即通道(channel)或叫深度(depth)
+    # each feature map should have one beta and one gamma
+    # 每一片卷积结果(feature map)都有一个beta值和一个gamma值
+    beta = tf.get_variable('beta',
+                         params_shape,
+                         initializer=tf.zeros_initializer)
 
-kitten, puppy = imread('kitten.jpg'), imread('puppy.jpg')
-# kitten is wide, and puppy is already square
-d = kitten.shape[1] - kitten.shape[0]
-kitten_cropped = kitten[:, d//2:-d//2, :]
+    gamma = tf.get_variable('gamma',
+                          params_shape,
+                          initializer=tf.ones_initializer)
 
-img_size = 200   # Make this smaller if it runs too slow
-x = np.zeros((2, 3, img_size, img_size))
-x[0, :, :, :] = imresize(puppy, (img_size, img_size)).transpose((2, 0, 1))
-x[1, :, :, :] = imresize(kitten_cropped, (img_size, img_size)).transpose((2, 0, 1))
+    # mean and variance during trianing are recorded and saved as moving_mean and moving_variance
+    # moving_mean and moving variance are used as mean and variance in testing.
+    # 训练过程中得出的平均值和方差都被记录下来，并被用来计算移动平均值(moving_mean)和移动方差(moving_variance)
+    # 移动平均值(moving_mean)和移动方差(moving_variance)将在预测阶段被使用
+    moving_mean = tf.get_variable('moving_mean',
+                                params_shape,
+                                initializer=tf.zeros_initializer,
+                                trainable=False)
+    moving_variance = tf.get_variable('moving_variance',
+                                    params_shape,
+                                    initializer=tf.ones_initializer,
+                                    trainable=False)
 
-# Set up a convolutional weights holding 2 filters, each 3x3
-w = np.zeros((2, 3, 3, 3))
+    # update variable by variable * decay + value * (1 - decay)
+    # 更新移动平均值和移动方差，更新方式是 variable * decay + value * (1 - decay)
+    update_moving_mean = moving_averages.assign_moving_average(moving_mean,
+                                                               mean, BN_DECAY)
+    update_moving_variance = moving_averages.assign_moving_average(
+        moving_variance, variance, BN_DECAY)
+    tf.add_to_collection(tf.GraphKeys.UPDATE_OPS, update_moving_mean)
+    tf.add_to_collection(tf.GraphKeys.UPDATE_OPS, update_moving_variance)
 
-# The first filter converts the image to grayscale.
-# Set up the red, green, and blue channels of the filter.
-w[0, 0, :, :] = [[0, 0, 0], [0, 0.3, 0], [0, 0, 0]]
-w[0, 1, :, :] = [[0, 0, 0], [0, 0.6, 0], [0, 0, 0]]
-w[0, 2, :, :] = [[0, 0, 0], [0, 0.1, 0], [0, 0, 0]]
+    mean, variance = control_flow_ops.cond(
+        is_training, lambda: (mean, variance),
+        lambda: (moving_mean, moving_variance))
 
-# Second filter detects horizontal edges in the blue channel.
-w[1, 2, :, :] = [[1, 2, 1], [0, 0, 0], [-1, -2, -1]]
 
-# Vector of biases. We don't need any bias for the grayscale
-# filter, but for the edge detection filter we want to add 128
-# to each output so that nothing is negative.
-b = np.array([0, 128])
+    a1_b = tf.nn.batch_normalization(a1, mean, variance, beta, gamma, BN_EPSILON)
+    # 2x2 Max Pooling layer with a stride of 2
+    # 2x2 的池化层，步长为2
+    m1 = tf.nn.max_pool(a1_b, ksize=[1,2,2,1], strides = [1,2,2,1], padding='VALID')
+    # shape of m1 should be batchsize * 26/2 * 26/2 * 32 = batchsize * 5408
+    # Affine layer with 1024 output units
+    # 池化后的结果m1的大小应为 batchsize * 26/2 * 26/2 * 32 = batchsize * 5408
+    # 仿射层共输出2014个值
+    m1_flat = tf.reshape(m1, [-1, 5408])
+    W1 = tf.get_variable("W1", shape=[5408, 1024])
+    b1 = tf.get_variable("b1", shape=[1024])
+    h2 = tf.matmul(m1_flat,W1) + b1
+    # ReLU Activation Layer
+    # ReLU激活层
+    a2 = tf.nn.relu(h2)
+    # Affine layer from 1024 input units to 10 outputs
+    # 仿射层有1024个输入和10个输出
+    W2 = tf.get_variable("W2", shape=[1024, 10])
+    b2 = tf.get_variable("b2", shape=[10])
+    y_out = tf.matmul(a2,W2) + b2
+    return y_out
 
-# Compute the result of convolving each input in x with each filter in w,
-# offsetting by b, and storing the results in out.
-out, _ = conv_forward_naive(x, w, b, {'stride': 1, 'pad': 1})
 
-def imshow_noax(img, normalize=True):
-    """ Tiny helper to show images as uint8 and remove axis labels """
-    if normalize:
-        img_max, img_min = np.max(img), np.min(img)
-        img = 255.0 * (img - img_min) / (img_max - img_min)
-    plt.imshow(img.astype('uint8'))
-    plt.gca().axis('off')
+y_out = complex_model(X,y,is_training)
 
-# Show the original images and the results of the conv operation
-plt.subplot(2, 3, 1)
-imshow_noax(puppy, normalize=False)
-plt.title('Original image')
-plt.subplot(2, 3, 2)
-imshow_noax(out[0, 0])
-plt.title('Grayscale')
-plt.subplot(2, 3, 3)
-imshow_noax(out[0, 1])
-plt.title('Edges')
-plt.subplot(2, 3, 4)
-imshow_noax(kitten_cropped, normalize=False)
-plt.subplot(2, 3, 5)
-imshow_noax(out[1, 0])
-plt.subplot(2, 3, 6)
-imshow_noax(out[1, 1])
-plt.show()
+# Now we're going to feed a random batch into the model
+# and make sure the output is the right size
+# 现在我们随机输入一个batch进入模型，来验证一下输出的大小是否如预期
+x = np.random.randn(64, 32, 32,3)
+with tf.Session() as sess:
+    with tf.device("/cpu:0"): #"/cpu:0" or "/gpu:0"
+        tf.global_variables_initializer().run()
 
+        ans = sess.run(y_out,feed_dict={X:x,is_training:True})
+        print(ans.shape)
+        print(np.array_equal(ans.shape, np.array([64, 10])))
